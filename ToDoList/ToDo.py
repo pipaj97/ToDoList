2440f9cb56d1c21c8fee7eacd73c2125f0f9644b
Julian Pipart <julian.pipart@fu-berlin.de>
2020-05-12 10:38:06 +0200

    update with failures

--- ToDoList/ToDo.py
+++ ToDoList/ToDo.py
@@ -1,13 +1,16 @@
 """
 A simple ToDo list
 """
+import pandas as pd
 
 # TODO:
-# - read csv
-# - write task
-# - add a task
-# - finish task and remove
-# - print tasks
+# BUGFIX:
+# - status ändert sich bei finish task
+# - index bei finish task = -1
+
+# - docstring ändern
+# - remove task
+# - unfish task
 
 
 """
@@ -25,31 +28,84 @@ dict
 
 """
 
-# TODO dict richtig zurückgeben
+
 def read_data(data_file):
-    with open(data_file) as f:
-        f.readline()
-        data = {}
-        for line in f:
-
-            (data["status"], data["task"]) = line.split(";")
-            # print(f"Your current tasks: \n{input_data}")
-        print(data)
-        # return data
+    df = pd.read_csv(data_file, sep=",")
+    return df
 
 
-def add_task(data_file):
-    with open(data_file, "a") as f:
-        new_task = input("Add a new task: \n")
-        new_task_to_append = f"\n[ ] {new_task}"
-        f.write(new_task_to_append)
+"""
+Adds a task to the end of the csv file.
+
+Parameters
+----------
+input file: str
+
+user input: str
+
+Returns
+-------
+data file: file
+
+"""
+
+
+def add_task(df):
+    new_task = input("Add a new task:\n")
+    df_mod = df.append({"status": 0, "task": new_task}, ignore_index=True)
+    csv_mod = df_mod.to_csv("../data/template.csv", index=False)
+    return csv_mod, output_data(df_mod)
+
+
+
+"""
+prints the data in the terminal
+
+Parameters
+----------
+input file: str
+
+Returns
+-------
+none
+
+"""
+
+
+def output_data(data):
+    data_out = data
+    print("Your tasks:\n")
+    data_out.loc[(data_out["status"] == 0), ["status"]] = "[ ]"
+    data_out.loc[(data_out["status"] == 1), ["status"]] = "[X]"
+    # data_out.index = data_out.index + 1
+    print(data_out)
+    # return data
+
+
+def finish_task(df):
+    # print(df)
+    task_to_finish = input("Which task should be finished?: \n")
+    task_to_finish = df.iloc[int(task_to_finish) - 1]
+    task_to_finish["status"] = 1
+    # print(task_to_finish)
+    csv_mod = df.to_csv("../data/template.csv", index=False)
+    return csv_mod
+
+    # csv_mod = task_to_finish.to_csv("../data/template.csv", index=False)
+    # return
+
+
 
 
 def main():
     test = "../data/template.csv"
-    read_data(test)
-    # add_task(test)
-    # read_data(test)
+    # data = read_data(test)
+    data = read_data(test)
+    test_data = read_data(test)
+    # print(data)
+    output_data(data)
+    # add_task(test_data)
+    finish_task(data)
 
 
 if __name__ == "__main__":
