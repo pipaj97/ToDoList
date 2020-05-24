d5c435803fbde60a195e33b87d3a5da7562bd3cf
Julian Pipart <julian@MacBook-Pro.local>
2020-05-24 16:55:48 +0200

    dynamic path and docstrings

--- ToDoList/ToDo.py
+++ ToDoList/ToDo.py
@@ -2,42 +2,35 @@
 A simple ToDo list
 """
 import pandas as pd
+from pathlib import Path
 
 class ToDo:
     """
     Driver class for the functionality of the ToDo list.
     """
     def __init__(self):
-        self.data_file = "/home/julian/PythonProjects/MyProjects/ToDoList/data/template.csv"
+    
+        self.data_file = self.get_path()
         self.df = pd.read_csv(self.data_file, sep=",")
-        print(self.greeting())
         self.output_data(self.df)
 
-    def greeting(self):
-        return r"""
- ______        ____            __                    __
-/\__  _\      /\  _`\         /\ \       __         /\ \__
-\/_/\ \/   ___\ \ \/\ \    ___\ \ \     /\_\    ____\ \ ,_\
-   \ \ \  / __`\ \ \ \ \  / __`\ \ \  __\/\ \  /',__\\ \ \/
-    \ \ \/\ \L\ \ \ \_\ \/\ \L\ \ \ \L\ \\ \ \/\__, `\\ \ \_
-     \ \_\ \____/\ \____/\ \____/\ \____/ \ \_\/\____/ \ \__\
-      \/_/\/___/  \/___/  \/___/  \/___/   \/_/\/___/   \/__/
 
-    Brought to you by @pipaj97 and @hugo_weizenkeim
-    """
-
+    def get_path(self):
+        base_path = Path(__file__).parent
+        file_path = (base_path / "../data/template.csv").resolve()
+        return file_path
 
     def output_data(self, df):
         """
-        Prints the data in the terminal.
+        Prints the DataFrame in the terminal.
 
         Parameters
         ----------
-        input file: str
+        input file: DataFrame
 
         Returns
         -------
-        none
+        None
         """
         data_out = df.copy()
         if data_out.empty:
@@ -57,13 +50,19 @@ class ToDo:
 
         Parameters
         ----------
-        input file: DataFrame
-
-        user input: str
+        None
 
         Returns
         -------
-        data file: DataFrame
+        If no user input is given:
+            self.output_data(self.df): func
+                Function to display the updated DataFrame.
+        
+        Else:
+            csv_mod: CSV
+                Updated CSV file.
+            self.output_data(df_mod): func
+                Function to display the updated DataFrame.
         """
         new_task = input("Add a new task:\n")
         if new_task == "":
@@ -71,7 +70,7 @@ class ToDo:
             return self.output_data(self.df)
         else:
             df_mod = self.df.append({"STATUS": 0, "TASK": new_task}, ignore_index=True)
-            csv_mod = df_mod.to_csv("/home/julian/PythonProjects/MyProjects/ToDoList/data/template.csv", index=False)
+            csv_mod = df_mod.to_csv(self.data_file, index=False)
             return csv_mod, self.output_data(df_mod)
 
 
@@ -81,39 +80,41 @@ class ToDo:
 
         Parameters
         ----------
-        input file: DataFrame
-
-        user input: str
+        chosen_task: dict of {str: str}
+            The chosen task which will be labelled as finished.
 
         Returns
         -------
-        data file: DataFrame
+        csv_mod: CSV
+            Updated CSV file.
+        self.output_data(df_TTF): func
+            Function to display the updated DataFrame.
         """
         df_TTF = self.df
-        task_to_finish = chosen_task
         df_TTF.loc[df_TTF["TASK"] == chosen_task, ["STATUS"]] = 1
-        csv_mod = df_TTF.to_csv("/home/julian/PythonProjects/MyProjects/ToDoList/data/template.csv", index=False)
+        csv_mod = df_TTF.to_csv(self.data_file, index=False)
         return csv_mod, self.output_data(df_TTF)
 
 
     def unfinish_task(self, chosen_task):
         """
-        Relabels a finished task as unfinished.
+        Labels a task as unfinished.
 
         Parameters
         ----------
-        input file: DataFrame
-
-        user input: str
+        chosen_task: dict of {str: str}
+            The chosen task which will be labelled as unfinished.
 
         Returns
         -------
-        data file: DataFrame
+        csv_mod: CSV
+            Updated CSV file.
+        self.output_data(df_TTU): func
+            Function to display the updated DataFrame.
         """
         df_TTU = self.df
-        task_to_finish = chosen_task
         df_TTU.loc[df_TTU["TASK"] == chosen_task, ["STATUS"]] = 0
-        csv_mod = df_TTU.to_csv("/home/julian/PythonProjects/MyProjects/ToDoList/data/template.csv", index=False)
+        csv_mod = df_TTU.to_csv(self.data_file, index=False)
         return csv_mod, self.output_data(df_TTU)
 
 
@@ -123,36 +124,18 @@ class ToDo:
 
         Parameters
         ----------
-        input file: DataFrame
-
-        user input: str
+        chosen_task: dict of {str: str}
+            The chosen task which will be removed.
 
         Returns
         -------
-        data file: DataFrame
+        csv_mod: CSV
+            Updated CSV file.
+        self.output_data(new_df_TTR): func
+            Function to display the updated DataFrame.
         """
         df_TTR = self.df
-        task_to_remove = chosen_task
         df_TTR = df_TTR.drop(df_TTR[df_TTR["TASK"] == chosen_task].index)
         new_df_TTR = df_TTR.reset_index(drop=True)
-        csv_mod = new_df_TTR.to_csv("/home/julian/PythonProjects/MyProjects/ToDoList/data/template.csv", index=False)
+        csv_mod = new_df_TTR.to_csv(self.data_file, index=False)
         return csv_mod, self.output_data(new_df_TTR)
-
-
-    def exit(self):
-        """
-        Enables not to change the ToDo list.
-
-        Parameters
-        ----------
-        input file: DataFrame
-
-        Returns
-        -------
-        data file: str
-        """
-        return print(
-        r"""
-༼ つ ◕_◕ ༽つ  Don't leave me alone! ༼ つ ◕_◕ ༽つ
-        """
-        )
